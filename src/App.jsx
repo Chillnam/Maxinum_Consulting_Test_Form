98a9b2d9c82924b13c91844f98d5490b9218e2f7
Alisher Saugabayev <alisher.saugabayev@alumni.nu.edu.kz>
2024-08-23 11:56:50 +0500

    Test again

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>Heloo alisherrrrr</div>
+      <div>Heloo alieeen</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
